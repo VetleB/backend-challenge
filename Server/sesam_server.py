25a3a12e2683773bb273564b20a08d3da1b48141
VetleB <vetle.birkeland@gmail.com>
2022-05-16 13:13:47 +0200

    Return excels

--- Server/sesam_server.py
+++ Server/sesam_server.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask_restful import Resource, Api, abort
 import os
 import json
+from tablib import Dataset
 
 app = Flask(__name__)
 api = Api(app)
@@ -35,8 +36,18 @@ class DatasetIds(Resource):
 
 
 class Excel(Resource):
-    def get(self):
-        return 1
+    def get(self, id):
+        check_id_exists(id)
+        temps = os.listdir("temp")
+        for t in temps:
+            os.remove("temp/{}".format(t))
+
+        with open('datasets/{}'.format(id), 'r') as f:
+            json_data = Dataset().load(f, format='json')
+            xls_data = json_data.export('xls')
+            with open('temp/{}.xls'.format(id[:-5]), 'wb') as f:
+                f.write(xls_data)
+            return send_file('temp/{}.xls'.format(id[:-5]))
 
 
 api.add_resource(Datasets, '/datasets')
