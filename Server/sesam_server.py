5507698f7ddc3f42879cdde7ae4d448d20ce266f
VetleB <vetle.birkeland@gmail.com>
2022-05-16 10:01:23 +0200

    Add url endpoints for API

--- Server/sesam_server.py
+++ Server/sesam_server.py
@@ -3,6 +3,26 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/datasets", methods=["GET"])
+def hello():
+    return "Hello, world!"
+
+
+@app.route("/datasets", methods=["POST"])
+def hello():
+    return "Hello, world!"
+
+
+@app.route("/datasets/<id>", methods=["GET"])
+def hello():
+    return "Hello, world!"
+
+
+@app.route("/datasets/<id>", methods=["DELETE"])
+def hello():
+    return "Hello, world!"
+
+
+@app.route("/datasets/<id>/excel", methods=["GET"])
 def hello():
     return "Hello, world!"
