6f6a46c4ff7b2ac74f99a5e3a50381d53275dd36
VetleB <vetle.birkeland@gmail.com>
2022-05-16 10:25:06 +0200

    Create branch using flask_restful

--- Server/sesam_server.py
+++ Server/sesam_server.py
@@ -1,28 +1,31 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/datasets", methods=["GET"])
-def hello():
-    return "Hello, world!"
+class Datasets(Resource):
+    def get(self):
+        return 1
+
+    def post(self):
+        return 1
 
 
-@app.route("/datasets", methods=["POST"])
-def hello():
-    return "Hello, world!"
+class DatasetIds(Resource):
+    def get(self, id):
+        return 1
+
+    def delete(self, id):
+        return 1
 
 
-@app.route("/datasets/<id>", methods=["GET"])
-def hello():
-    return "Hello, world!"
+class Excel(Resource):
+    def get(self):
+        return 1
 
 
-@app.route("/datasets/<id>", methods=["DELETE"])
-def hello():
-    return "Hello, world!"
-
-
-@app.route("/datasets/<id>/excel", methods=["GET"])
-def hello():
-    return "Hello, world!"
+api.add_resource(Datasets, '/datasets')
+api.add_resource(DatasetIds, '/datasets/<id>')
+api.add_resource(Excel, '/datasets/<id>/excel')
