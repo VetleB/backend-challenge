7203843704ec8579d7c27573b5cfa15a8d5cb50f
VetleB <vetle.birkeland@gmail.com>
2022-05-16 12:21:14 +0200

    Return specific id

--- Server/sesam_server.py
+++ Server/sesam_server.py
@@ -1,28 +1,41 @@
-from flask import Flask
+import os
+import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
+def check_id_exists(id):
+    ids = os.listdir("datasets")
+    return id in ids
+
+
 @app.route("/datasets", methods=["GET"])
-def hello():
+def get_entries():
     return "Hello, world!"
 
 
 @app.route("/datasets", methods=["POST"])
-def hello():
+def create_entry():
+    new_data = json.loads(request.data)
     return "Hello, world!"
 
 
 @app.route("/datasets/<id>", methods=["GET"])
-def hello():
-    return "Hello, world!"
+def get_entry(id):
+    if check_id_exists(id):
+        with open("datasets/{}".format(id), 'r') as f:
+            data = json.load(f)
+            return jsonify(data)
+    else:
+        return '', 404
 
 
 @app.route("/datasets/<id>", methods=["DELETE"])
-def hello():
+def delete_entry():
     return "Hello, world!"
 
 
 @app.route("/datasets/<id>/excel", methods=["GET"])
-def hello():
+def get_excel():
     return "Hello, world!"
