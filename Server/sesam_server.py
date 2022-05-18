68a82370045141a6735073ba8e8070ebabd911c5
VetleB <vetle.birkeland@gmail.com>
2022-05-18 03:00:30 +0200

    Change id to jid

--- Server/sesam_server.py
+++ Server/sesam_server.py
@@ -66,50 +66,50 @@ class Datasets(Resource):
 
 
 class DatasetIds(Resource):
-    def get(self, id):
+    def get(self, jid):
         """
         Fetch the dataset entry matching the id
 
-        :param id: Id of the entry to be fetched
+        :param jid: Id of the entry to be fetched
         :return: JSON object
         """
-        check_name_exists(id)
-        with open('datasets/{}'.format(id), 'r') as f:
+        check_name_exists(jid)
+        with open('datasets/{}'.format(jid), 'r') as f:
             data = json.load(f)
             return data, 200
 
-    def delete(self, id):
+    def delete(self, jid):
         """
         Delete dataset entry matching the id
 
-        :param id: Id of entry to be deleted
+        :param jid: Id of entry to be deleted
         :return:
         """
-        check_name_exists(id)
-        os.remove("datasets/{}".format(id))
+        check_name_exists(jid)
+        os.remove("datasets/{}".format(jid))
         return 204
 
 
 class Excel(Resource):
-    def get(self, id):
+    def get(self, jid):
         """
         Fetch the dataset entry matching the id and convert it to a .xls file
 
-        :param id: Id of the entry to be fetched
+        :param jid: Id of the entry to be fetched
         :return: .xls file
         """
-        check_name_exists(id)
+        check_name_exists(jid)
 
         temps = os.listdir("temp")
         for t in temps:
             os.remove("temp/{}".format(t))
 
-        with open('datasets/{}'.format(id), 'r') as f:
+        with open('datasets/{}'.format(jid), 'r') as f:
             json_data = Dataset().load(f, format='json')
             xls_data = json_data.export('xls')
-            with open('temp/{}.xls'.format(id[:-5]), 'wb') as tmp:
+            with open('temp/{}.xls'.format(jid[:-5]), 'wb') as tmp:
                 tmp.write(xls_data)
-            return send_file('temp/{}.xls'.format(id[:-5])), 200
+            return send_file('temp/{}.xls'.format(jid[:-5])), 200
 
 
 api.add_resource(Datasets, '/datasets')
